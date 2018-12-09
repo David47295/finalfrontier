94c94a33ae5bf884639f45efe62154b4543c6e96
David Wan <davidwan97@gmail.com>
2018-12-08 20:45:21 -0500

    Initial commit -m

---
+++ FinalFrontier/Source/FinalFrontier/FinalFrontierGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FinalFrontierGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FINALFRONTIER_API AFinalFrontierGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
